984e3c4434d282a744b92c410f830fc5abe87f0f
Berkay Askin <berkayaskiin@gmail.com>
2018-08-08 12:28:56 +0000

    authentication

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    User        = require("./models/user"),
-    seedDB      = require("./seeds")
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    seedDB          = require("./seeds")
 
 mongoose.connect('mongodb://localhost:27017/campgrounds', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,18 +15,36 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "Rusty is the cutest dog!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+   res.locals.currentUser = req.user;
+   next();
+});
+
 
 app.get("/", function(req, res){
    res.render("landing");
 });
 
+//INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res){
     //Get all campgrounds from DB
     Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err);
         } else {
-            res.render("campgrounds/index", {campgrounds:allCampgrounds});
+            res.render("campgrounds/index", {campgrounds:allCampgrounds, currentUser: req.user});
         }
     });
 });
@@ -67,7 +87,7 @@ app.get("/campgrounds/:id", function(req, res) {
 //   COMMENTS ROUTES
 // ===================
 
-app.get("/campgrounds/:id/comments/new", function(req, res) {
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
    ///find campground by id
    Campground.findById(req.params.id, function(err, campground){
       if(err){
@@ -78,7 +98,7 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
    });
 });
 
-app.post("/campgrounds/:id/comments", function(req, res){
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
    //lookup campground using ID
    Campground.findById(req.params.id, function(err, campground) {
       if(err){
@@ -99,9 +119,53 @@ app.post("/campgrounds/:id/comments", function(req, res){
           });
       }
    });
-   
-   
 });
+
+//============
+//AUTH ROUTES
+//============
+
+//SHOW REGISTER FORM
+app.get("/register", function(req, res){
+   res.render("register") ;
+});
+//hande sign up logic
+app.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+        console.log(err);
+        return res.render("register");
+    }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        })
+    });
+});
+
+//show login form
+app.get("/login", function(req, res) {
+   res.render("login"); 
+});
+//hande login logic(middleware)
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"    
+    }), function(req, res) {
+});
+//logout route
+app.get("/logout", function(req, res) {
+   req.logout();
+   res.redirect("/campgrounds");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Campgrounds Server has started.");
--- commit-notes.md
+++ commit-notes.md
@@ -2,9 +2,9 @@ COMMIT NOTES DAILY
 
 FIRST DAY - FIRST COMMIT
 Date: 25 July 2018
-CHAPTER 26 - BASICS
+SECTION 26 - BASICS
 
-#INITIAL ROUTES
+* INITIAL ROUTES
 
 1. Started with “npm init” to create package.json. 
 2. “npm install express ejs —save”
@@ -12,40 +12,40 @@ CHAPTER 26 - BASICS
 4. Add landing page
 5. Add campgrounds page that lists all campgrounds. Each Campground has name and image
 
-#LAYOUT AND BASIC STYLING
+* LAYOUT AND BASIC STYLING
 
 1. Create header and footer partials
 2. Add in Bootstrap
 
-#CREATING CAMPGROUNDS
+* CREATING CAMPGROUNDS
 
 1. Setup new campground POST route
 2. Add in body-parser
 3. Setup route to show form
 4. Add basic unstyled form
 
-#STYLE THE CAMPGROUNDS PAGE
+* STYLE THE CAMPGROUNDS PAGE
 
 1. Add a better header/title (jumbotron)
 2. Make campgrounds display in a grid
 
-#STYLE THE NAVBAR AND FORM
+* STYLE THE NAVBAR AND FORM
 
 1. Add a navbar to all templates
 2. Style the new campground form
 
 SECOND COMMIT
 Date: 27 July 2018
-CHAPTER 28: DATA PERSISTANCE
+SECTION 28: DATA PERSISTANCE
 
-#ADDING MONGOOSE
+* ADDING MONGOOSE
 
 1. Install and configure mongoose
 2. Setup campground model
 3. Use campground model inside of our routes
 
 Date: 28 July 2018
-#SHOW PAGE
+* SHOW PAGE
 
 1. Add description to our campground model
 2. Show db.collection.drop()
@@ -55,31 +55,55 @@ Date: 28 July 2018
 
 THIRD COMMIT
 Date: 02 August  2018
-CHAPTER 31: COMMENTS
+SECTION 31: COMMENTS
 
-#REFACTOR MONGOOSE CODE
+* REFACTOR MONGOOSE CODE
 
 1. Create a models directory
 2. Use module.exports
 3. Require everything correctly
 
-#ADD SEEDS FILE
+* ADD SEEDS FILE
 
 1. Add a seeds.js file
 2. Run the seeds file every time the server starts
 
-#ADD THE COMMENT MODEL
+* ADD THE COMMENT MODEL
 1. Handle the errors
 2. Display comments on campground show page
  
 Date: 03 August 2018
-#COMMENT NEW/CREATE
+* COMMENT NEW/CREATE
 1. Add the comment, new and create routes(nested routes)
 2. Add the new comment form
 
 Date: 06 August 2018
-#STYLE SHOW PAGE
+* STYLE SHOW PAGE
 1. Add sidebar to show page
 2. Styling display comments 
 
+FOURTH COMMIT
+Date: 08 August 2018
+SECTION 33: ADDING AUTHENTICATION
 
+* AUTH PT. 1 - ADD USER MODEL
+1. Install all packages for auth(passport.js)
+2. Define User model
+
+* AUTH PT. 2 - REGISTER
+1. Configure Passport
+2. Add register routes
+3. Add register template
+
+* AUTH PT. 3 - LOGIN
+1. Add login routes
+2. Add login template
+
+* AUTH PT. 4 - LOGOUT/NAVBAR
+1. Add logout route
+2. Prevent user from adding a comment if not signed in
+3. Add links to navbar
+4. Show/hide auth links correctly
+
+* AUTH PT.5 - SHOW/HIDE LINKS
+1. Show/hide auth links in navbar correctly
